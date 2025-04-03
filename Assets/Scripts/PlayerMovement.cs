7995a302e0036c3dd38f9afde6c6ddc4cecf00d1
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-03 18:28:38 +0300

    added hor movin + run anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,44 @@
+using System;
 using UnityEngine;
 
-public class PlayerMovement
+/// <summary>
+/// Скрипт обработки перемещений персонажа игрока
+/// </summary>
+public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        // 
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
+        }
+        
+        
+        anim.SetBool("RunNormal", horizontalInput != 0);
+    }
 }
