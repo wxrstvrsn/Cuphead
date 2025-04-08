1f86f69d1f7e0e242929c2d780c7a81bedd43e8b
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-09 01:59:51 +0300

    deeper decomposing and smth else... idk

---
+++ Assets/Scripts/EntityAnimation.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class EntityAnimation : MonoBehaviour
+{
+    protected Animator _anim;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public virtual void SetRunning(bool isRunning)
+    {
+        _anim.SetBool("running", isRunning);
+    }
+    
+    public virtual void SetGrounded(bool isGrounded)
+    {
+        _anim.SetBool("grounded", isGrounded);
+    }
+    
+    public virtual void PlayJump()
+    {
+        Debug.Log(">>> Jump Triggered");
+        _anim.ResetTrigger("jump");
+        _anim.SetTrigger("jump");
+    }
+
+    public virtual void UpdateShootingAnimation(bool isShooting, bool isRunning)
+    {
+        _anim.SetBool("shStraight", isShooting && !isRunning);
+    }
+}
--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,33 +1,9 @@
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnimation : EntityAnimation
 {
-    private Animator _anim;
-
-    private void Awake()
+    public void PlayDash()
     {
-        _anim = GetComponent<Animator>();
-    }
-
-    public void SetRunning(bool isRunning)
-    {
-        _anim.SetBool("running", isRunning);
-    }
-
-    public void SetGrounded(bool isGrounded)
-    {
-        _anim.SetBool("grounded", isGrounded);
-    }
-
-    public void PlayJump()
-    {
-        Debug.Log(">>> Jump Triggered");
-        _anim.ResetTrigger("jump");
-        _anim.SetTrigger("jump");
-    }
-
-    public void UpdateShootingAnimation(bool isShooting, bool isRunning)
-    {
-        _anim.SetBool("shStraight", isShooting && !isRunning);
+        _anim.SetTrigger("dash");
     }
 }
