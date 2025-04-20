3c2358572a1874267999b839a464e80eebccf0a2
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-21 02:50:11 +0300

    Reorganize lil bit + smol ;) changs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,21 +4,24 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
-        
+
         print("CLICKED--------------------------------------------------------------");
     }
 
     public void Home()
     {
-        SceneManager.LoadScene("Main Menu");
-        
-        Time.timeScale = 1;
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
+
 
     public void Resume()
     {
@@ -30,7 +33,7 @@ public class PauseMenu : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        
+
         Time.timeScale = 1;
     }
 }
