d9d0a5ee9f34c87c446cc2be5cada2c6a47b6cb9
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-21 21:31:57 +0300

    Need to read docs 'bout "Coroutine"

--- Assets/Scripts/Enemies/Bosses/Boss.cs
+++ Assets/Scripts/Enemies/Bosses/Boss.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
@@ -6,14 +7,50 @@ public abstract class Boss : Entity, IDamageable
     [Header("Boss Settings")]
     [SerializeField] protected int maxHealth;
     [SerializeField] protected float phaseTransitionDelay = 2f;
-
+    
+    
+    private Coroutine _stateCoroutine;
 
     // TODO:
-    //      FSM - ???? мож попробовать?? 
-    
+    //      FSM - ???? мож попробовать??
+    //      «Конечный  автомат — это простая структура, позволяющая определять сложное поведение» - о как
+    protected enum State
+    {
+        Intro,
+        Phase1,
+        Phase2,
+        Dead
+    }
+    protected State _state;
+
+    protected virtual void Awake()
+    {
+        //
+    }
+
+    protected void EnterState(State state)
+    {
+        if(_stateCoroutine != null)
+            StopCoroutine(_stateCoroutine);
+        
+        _state = state;
+
+        switch (state)
+        {
+            case State.Intro:
+                _stateCoroutine = StartCoroutine(PlayIntro());
+        }
+    }
+
+    private IEnumerator PlayIntro()
+    {
+        throw new NotImplementedException();
+    }
+
+
     public void GetDamage()
     {
-        throw new System.NotImplementedException();
+        
     }
     
     
