a3fe037c89c21cc107ea793e4ca9dc9dd0266ff1
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-23 00:01:39 +0300

    Working LaunchCarrots(): in |-- Correct Object Pooling |-- |> Incorrect: rotation of bullet when start to aimed moving |-- ? Need to add handle of Damaging -- give by bullet + take by Player

--- Assets/Scripts/Enemies/Bosses/CarrotBoss.cs
+++ Assets/Scripts/Enemies/Bosses/CarrotBoss.cs
@@ -1,113 +1,26 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
-public class CarrotBoss : Boss
+public class CarrotBoss : MonoBehaviour
 {
-    [Header("CarrotBoss Settings")] [SerializeField]
-    private float introDuration = 2f;
-
-    [SerializeField] private float laserDuration = 3f;
-    [SerializeField] private float carrotLaunchCooldown = 1f;
-
-    [Header("Attack References")] [SerializeField]
-    private Transform[] firePoints;
-
+    [SerializeField] private Transform[] firePoints;
     [SerializeField] private CarrotBulletPool bulletPool;
     [SerializeField] private Transform target;
-    [SerializeField] private float carrotBulletSpeed = 5f;
-
-    private CarrotAnimation _carrotAnimation;
-    private CarrotBossState _currentState;
-
-    private enum CarrotBossState
-    {
-        Intro,
-        LaunchCarrots,
-        LaserShoot,
-        Death
-    }
-
-    protected override void Awake()
-    {
-        base.Awake();
-        _carrotAnimation = GetComponent<CarrotAnimation>();
-    }
 
     private void Start()
     {
-        StartCoroutine(StateMachine());
+        StartCoroutine(FireCarrotsRoutine());
     }
 
-    private IEnumerator StateMachine()
+    private IEnumerator FireCarrotsRoutine()
     {
-        yield return DoIntro();
-
-        while (_currentState != CarrotBossState.Death)
+        while (true)
         {
-            CarrotBossState nextState =
-                Random.Range(0, 2) == 0 ? CarrotBossState.LaunchCarrots : CarrotBossState.LaserShoot;
-
-            switch (nextState)
-            {
-                case CarrotBossState.LaunchCarrots:
-                    _carrotAnimation.PlayLaunchCarrots();
-                    yield return LaunchCarrots();
-                    break;
-                case CarrotBossState.LaserShoot:
-                    _carrotAnimation.PlayLaser();
-                    yield return LaserShoot();
-                    break;
-            }
-
             yield return new WaitForSeconds(2f);
+
+            var bullet = bulletPool.GetBullet();
+            bullet.transform.position = firePoints[Random.Range(0, firePoints.Length)].position;
+            bullet.Launch(target);
         }
-
-        yield return DeathSequence();
-    }
-
-    private IEnumerator DoIntro()
-    {
-        _currentState = CarrotBossState.Intro;
-        _carrotAnimation.PlayIntro();
-        yield return new WaitForSeconds(introDuration);
-    }
-
-    private IEnumerator LaunchCarrots()
-    {
-        _currentState = CarrotBossState.LaunchCarrots;
-
-        for (int i = 0; i < 5; i++)
-        {
-            var carrot = bulletPool.GetCarrot();
-            Transform spawnPoint = firePoints[Random.Range(0, firePoints.Length)];
-
-            carrot.transform.position = spawnPoint.position;
-            carrot.Activate(target, carrotBulletSpeed);
-        }
-
-        yield return new WaitForSeconds(carrotLaunchCooldown);
-    }
-
-    private IEnumerator LaserShoot()
-    {
-        _currentState = CarrotBossState.LaserShoot;
-        yield return new WaitForSeconds(laserDuration);
-    }
-
-    public void Die()
-    {
-        if (_currentState != CarrotBossState.Death)
-        {
-            StopAllCoroutines();
-            _currentState = CarrotBossState.Death;
-            StartCoroutine(DeathSequence());
-        }
-    }
-
-    private IEnumerator DeathSequence()
-    {
-        _carrotAnimation.PlayDeath();
-        yield return new WaitForSeconds(3f);
-        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Enemies/Bosses/CarrotBullet.cs
+++ Assets/Scripts/Enemies/Bosses/CarrotBullet.cs
@@ -14,15 +14,34 @@ public class CarrotBullet : MonoBehaviour
         _isFlying = true;
         gameObject.SetActive(true);
 
-        // Через секунду меняем траекторию
-        Invoke(nameof(StartHoming), 1f);
+        // Через секунду прицеливаемся в игрока
+        Invoke(nameof(StartHoming), 2f);
     }
+
 
     private void StartHoming()
     {
-        if (_target == null) return;
-        _direction = (_target.position - transform.position).normalized;
+        if (_target == null)
+        {
+            Debug.LogWarning("Нет цели для наведения у морковки!");
+            return;
+        }
+
+        Vector2 toTarget = (_target.position - transform.position);
+
+        // Проверка: не лететь "вверх", если игрок уже над нами
+        if (toTarget.y > 0)
+        {
+            Debug.Log("Цель выше пули, сохраняем направление вниз.");
+            return; // не меняем направление
+        }
+
+        _direction = toTarget.normalized;
+
+        // Для красоты: морковка поворачивается в сторону
+        transform.up = _direction;
     }
+
 
     private void Update()
     {
